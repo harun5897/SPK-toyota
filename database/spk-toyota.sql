2b4edf502bf1e7e85fde4fd13c5d78dfcd65bcef
harun5897 <harungb025@gmail.com>
2022-08-25 19:15:13 +0700

    feat(database): update database

--- database/spk-toyota.sql
+++ database/spk-toyota.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Aug 08, 2022 at 07:15 PM
+-- Generation Time: Aug 25, 2022 at 02:14 PM
 -- Server version: 10.4.14-MariaDB
 -- PHP Version: 7.2.33
 
@@ -20,6 +20,66 @@ SET time_zone = "+00:00";
 --
 -- Database: `spk-toyota`
 --
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `customers`
+--
+
+CREATE TABLE `customers` (
+  `idCustomer` int(9) NOT NULL,
+  `namaDepan` varchar(255) NOT NULL,
+  `namaBelakang` varchar(255) NOT NULL,
+  `nomorPolisi` varchar(255) NOT NULL,
+  `nomorRangka` varchar(255) NOT NULL,
+  `merkKendaraan` varchar(255) NOT NULL,
+  `tipeKendaraan` varchar(255) NOT NULL,
+  `kontak` varchar(255) NOT NULL,
+  `alamat` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `customers`
+--
+
+INSERT INTO `customers` (`idCustomer`, `namaDepan`, `namaBelakang`, `nomorPolisi`, `nomorRangka`, `merkKendaraan`, `tipeKendaraan`, `kontak`, `alamat`) VALUES
+(2, 'Harun', 'Abdullah', 'df8787gf', '123456789', 'Mitsubishi', 'XPANDER', '08125659743', 'Tanjung Pinang'),
+(3, 'Aprilia', 'Figo', 'bp4567bg', '123456789', 'Toyota', 'Yaris 2017', '98765432', 'Batam Kota');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `kriteria`
+--
+
+CREATE TABLE `kriteria` (
+  `idKriteria` int(9) NOT NULL,
+  `namaKriteria` varchar(255) NOT NULL,
+  `bobotKriteria` varchar(255) NOT NULL,
+  `pertanyaanKriteria` varchar(255) NOT NULL,
+  `costBenefit` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `kriteria`
+--
+
+INSERT INTO `kriteria` (`idKriteria`, `namaKriteria`, `bobotKriteria`, `pertanyaanKriteria`, `costBenefit`) VALUES
+(2, 'Kriteria 1', '80', 'apakah ada ini ?', 'benefit');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `service`
+--
+
+CREATE TABLE `service` (
+  `idService` int(9) NOT NULL,
+  `idCustomer` int(9) NOT NULL,
+  `permasalahanKendaraan` varchar(255) NOT NULL,
+  `tanggalService` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 -- --------------------------------------------------------
 
@@ -47,6 +107,24 @@ INSERT INTO `users` (`idUser`, `username`, `email`, `password`, `role`) VALUES
 --
 
 --
+-- Indexes for table `customers`
+--
+ALTER TABLE `customers`
+  ADD PRIMARY KEY (`idCustomer`);
+
+--
+-- Indexes for table `kriteria`
+--
+ALTER TABLE `kriteria`
+  ADD PRIMARY KEY (`idKriteria`);
+
+--
+-- Indexes for table `service`
+--
+ALTER TABLE `service`
+  ADD PRIMARY KEY (`idService`);
+
+--
 -- Indexes for table `users`
 --
 ALTER TABLE `users`
@@ -55,6 +133,24 @@ ALTER TABLE `users`
 --
 -- AUTO_INCREMENT for dumped tables
 --
+
+--
+-- AUTO_INCREMENT for table `customers`
+--
+ALTER TABLE `customers`
+  MODIFY `idCustomer` int(9) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+
+--
+-- AUTO_INCREMENT for table `kriteria`
+--
+ALTER TABLE `kriteria`
+  MODIFY `idKriteria` int(9) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+
+--
+-- AUTO_INCREMENT for table `service`
+--
+ALTER TABLE `service`
+  MODIFY `idService` int(9) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
 
 --
 -- AUTO_INCREMENT for table `users`
